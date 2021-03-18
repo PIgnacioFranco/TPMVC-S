cdb8c0cd0986c957b6232d428703ced6273cab46
pignaciofranco <franco.p.ignacio@gmail.com>
2021-03-17 23:20:04 -0300

    falta

--- models/persona.js
+++ models/persona.js
@@ -3,14 +3,14 @@
 module.expport = {
 	guardarUnaPersona: async persona => {
 		let resultado = await conexion.query(
-			INSER INT persona (nombre, apellido. nickname, edad, email) VALUES (?,?,?,?,?)',
-			[persona.nombre, persona.apellido, persona.nickname, persona.edad, persona.email];
+			'INSERT INT persona (nombre, apellido. nickname, edad, email) VALUES (?,?,?,?,?)',
+			[persona.nombre, persona.apellido, persona.nickname, persona.edad, persona.email]);
 		
 		return resltado.insertId;
 	},
 	traerUnPersona: async id => {
 		let unaPersona = await conexion.query(
-			'SELECT * FROM persona WHERE id=?', [id];
+			'SELECT * FROM persona WHERE id=?', [id]);
 			
 		return unaPersona[0];
 	},
@@ -20,16 +20,15 @@ module.expport = {
 			
 		return listadoPersona;	
 	},
-	modificarPersona: async (id, edad, email) {
+	modificarPersona: async (id, edad, email) => {
 		let resultado = await conexion.query(
 			'UPDATE persona set edad=?, email=? WHERE id=?',
-			[edad, mail, id];
+			[edad, mail, id]);
 			
 		return resultado.changeRown;
 	},
 	borrarPersona: async id => {
 		let fecha = new Date();
-		let resultado = await conexion.query('UPDATE persona SET deleted = ?, date_teleteç
-		let resultado.affectedRows;
-	}
+		let resultado = await conexion.query('UPDATE persona SET deleted = ?, date_delete = ? WHERE id = ?', [persona.delete, fecha, persona.id]);
+		}		
 }
